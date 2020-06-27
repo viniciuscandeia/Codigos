e1c9fe71498dfab9d8ea213976bd9371ca43ba62
Vinicius Candeia <53703195+viniciuscandeia@users.noreply.github.com>
2020-06-27 14:31:40 -0300

    Add files via upload

---
+++ 01 - Deixando tudo pronto.py
@@ -0,0 +1,6 @@
+print('\033[35m{:=^40}\033[m'.format('='))
+print('\033[35m{:^40}\033[m'.format(' DEIXANDO TUDO PRONTO '))
+print('\033[35m{:=^40}\033[m'.format('='))
+
+print('\033[31m{:^40}\033[m'.format('Olá, mundo!'))
+
